f717615e2e286970873a7931c221122b4ca5c66b
Saf Petra <new.petra01@student.uns.ac.id>
2022-06-07 10:38:37 +0700

    mengerjakan exercise 11

---
+++ Exercise-11/Data/Geometri2D.py
@@ -0,0 +1,18 @@
+# Silakan perhatikan perintah pada file 11_Modul&Paket.ipynb sebelum membuka file ini
+import math
+
+#fungsi luas persegi panjang
+def LuasPersegiPanjang (p,l):
+    return p*l
+
+#fungsi kelliling persegi panjang
+def KelilingPersegiPanjang (p,l):
+    return 2*(p+l)
+
+#fungsi luas bujur sangkar
+def LuasBujurSangkar (s):
+    return s*s
+
+#fungsi keliling bujur sangkar
+def KelilingBujurSangkar (s):
+    return 4*s
